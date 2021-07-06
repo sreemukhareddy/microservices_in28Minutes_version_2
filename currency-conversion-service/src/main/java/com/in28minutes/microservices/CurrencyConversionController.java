351c58a4931d74a215813a0a04e14e93b99d8d07
sreemukhareddy <rsreemukha@gmail.com>
2021-07-06 17:47:40 +0530

    circuit break

--- currency-conversion-service/src/main/java/com/in28minutes/microservices/CurrencyConversionController.java
+++ currency-conversion-service/src/main/java/com/in28minutes/microservices/CurrencyConversionController.java
@@ -5,6 +5,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.retry.annotation.Retry;
 import lombok.extern.log4j.Log4j2;
 
 @Log4j2
@@ -21,8 +23,14 @@ public class CurrencyConversionController {
 	}
 	
 	@GetMapping("feign/from/{from}/to/{to}/{quantity}")
+	@CircuitBreaker(name = "default", fallbackMethod = "currencyExchangeFallBack") // various settings are there, go through in28minutes
 	public ExchangeValue getExchangeValueFeign(@PathVariable String from, @PathVariable String to, @PathVariable Integer quantity) {
 		log.info("From -> {}, TO -> {}, Quantity -> {}", from, to, quantity);
 		return service.getExchangeValueFeign(from, to, quantity);
 	}
+	
+	public ExchangeValue currencyExchangeFallBack(Exception ex) {
+		return new ExchangeValue(null, ex.getMessage(), null, null, null, null, null); 
+	}
+
 }
